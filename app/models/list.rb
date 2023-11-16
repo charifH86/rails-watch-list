cb6224ae7b8babcb0f8b768280908478b9c3cedf
Charif Hassouni <hassounimohamedc@gmail.com>
2023-11-16 14:20:58 +0100

    C2modeldone
    Signed-off-by: Charif Hassouni <hassounimohamedc@gmail.com>

---
+++ app/models/list.rb
@@ -0,0 +1,12 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks
+  validates :name, presence: true, uniqueness: true
+  before_destroy :destroy_child_saved_movies
+
+  private
+
+  def destroy_child_saved_movies
+    movies.destroy_all
+  end
+end
